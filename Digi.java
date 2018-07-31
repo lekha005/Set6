816beba3100e25db9e793005f2ddcbf5daa90326
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 04:12:38 +0000

    Create Digi.java

---
+++ Digi.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Digi
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a=in.next();
+        System.out.print(a.length());
+}
+}
+
+
+
+
+
+
