3f4fe5a73492baf1531527be196e36e5d299d5aa
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 03:57:57 +0000

    Create Mul.java

---
+++ Mul.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Mul
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int m=in.nextInt();
+        if((n*m)%2==0)
+        System.out.print("even");
+        else
+        System.out.print("odd");
+
+}
+}
+
+
+
+
+
+
