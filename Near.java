344c9f30caaed4bc0314f5df4e9ef239e371e294
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 03:54:44 +0000

    Create Near.java

---
+++ Near.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Near
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n%2!=0)
+        System.out.print(n-1);
+        else
+        System.out.print(n);
+
+}
+}
+
+
+
+
+
+
