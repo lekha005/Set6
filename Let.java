f6917df88a4eb6c66957ceb7052cb9748dbbda3f
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 03:48:49 +0000

    Create Let.java

---
+++ Let.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Let
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        String[] a={"one","two","there","four","five","six","seven","eight","nine","ten"}; 
+        System.out.print(a[n-1]);
+}
+}
+
+
+
+
+
