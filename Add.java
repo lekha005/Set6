be24d92ea5a945b03322bd3b16218dd1298a3414
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-31 03:52:12 +0000

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Add
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n%9==0)
+        System.out.print("9");
+        else
+        System.out.print(n%9);
+
+}
+}
+
+
+
+
+
+
